3151e58154ebd748e556c38bd6629365db3c919d
Supriya Gole <supriyag@thoughtworks.com>
2016-11-30 12:52:44 +0530

    implemented repl for my language.

--- grammar/js/repl.js
+++ grammar/js/repl.js
@@ -1,15 +1,16 @@
 const readline = require('readline');
+var chalk = require('chalk');
 var processTree = require('/Users/supriyag/workspace/jison/grammar/js/assignment3.js').process;
 var lookupTable = require('../js/lookupTable');
 var Parser = require('jison').Parser;
 var fs = require('fs');
-var grammar = fs.readFileSync('./grammar/lex/assignment3.jison','utf-8');
-var parser = new Parser(grammar);
-var identifierStorage = new lookupTable();
+var grammar = fs.readFileSync('./grammar/lex/assignment3.jison', 'utf-8');
 
+var identifierStorage = new lookupTable();
 var main = function (expression) {
+    var parser = new Parser(grammar);
     var trees = parser.parse(expression);
-    return processTree(trees,identifierStorage);
+    return processTree(trees, identifierStorage);
 };
 
 const rl = readline.createInterface({
@@ -21,7 +22,16 @@ const rl = readline.createInterface({
 rl.prompt();
 
 rl.on('line', function (line) {
-    console.log(main(line));
+    try {
+        var result = chalk.yellow(main(line));
+        console.log(result);
+    } catch (e) {
+        if (e.stack)
+            console.log(chalk.red(e.stack));
+        else
+            console.log(chalk.red(e.message));
+    }
+
     rl.prompt();
 }).on('close', function () {
     rl.write('(To exit, press ^C again or type .exit)');
